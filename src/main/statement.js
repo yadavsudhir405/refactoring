c5fcbf29167b52023759da94809f310ff7748f86
Sudhir <sudhir@yadavsudhir.com>
2021-04-21 13:48:42 +0530

    Accomodate render statement as html

--- src/main/statement.js
+++ src/main/statement.js
@@ -1,10 +1,10 @@
 import {createStatementData} from "./create-statement-data";
 
 export function statement(invoice, plays) {
-    return statementAsText(createStatementData(invoice, plays));
+    return renderAsText(createStatementData(invoice, plays));
 }
 
-function statementAsText(statementData) {
+function renderAsText(statementData) {
     let result = `Statement for ${statementData.customer}\n`;
     for (let perf of statementData.performances) {
         result += `  ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)\n`;
@@ -12,11 +12,30 @@ function statementAsText(statementData) {
     result += `Amount owed is ${usd(statementData.totalAmount / 100)}\n`;
     result += `You earned ${statementData.volumeCredits} credits\n`;
     return result;
+}
 
-    function usd(amount) {
-        return new Intl.NumberFormat("en-US",
-            { style: "currency", currency: "USD",
-                minimumFractionDigits: 2 }).format(amount)
+export function statementAsHtml(invoice, plays) {
+
+    return renderAsHtml(createStatementData(invoice, plays))
+}
+
+function renderAsHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
     }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.volumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+function usd(amount) {
+    return new Intl.NumberFormat("en-US",
+        { style: "currency", currency: "USD",
+            minimumFractionDigits: 2 }).format(amount)
 }
 
--- src/main/statement.spec.js
+++ src/main/statement.spec.js
@@ -1,7 +1,7 @@
-import {statement} from "./statement";
+import {statement, statementAsHtml} from "./statement";
 
 describe('Statement', () => {
-    it('should return sum', () => {
+    it('should render statements as plain text', () => {
         const invoice = {
             "customer": "BigCo",
             "performances": [
@@ -32,5 +32,39 @@ describe('Statement', () => {
             'You earned 47 credits\n';
 
         expect(statement(invoice, play)).toEqual(expectedStatement);
+    });
+    it('should render statements as html', () => {
+        const invoice = {
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        }
+        const play = {
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        }
+        const expectedStatement = "<h1>Statement for BigCo</h1>\n" +
+            "<table>\n" +
+            "<tr><th>play</th><th>seats</th><th>cost</th></tr>  <tr><td>Hamlet</td><td>55</td><td>$65,000.00</td></tr>\n" +
+            "  <tr><td>As You Like It</td><td>35</td><td>$58,000.00</td></tr>\n" +
+            "  <tr><td>Othello</td><td>40</td><td>$50,000.00</td></tr>\n" +
+            "</table>\n" +
+            "<p>Amount owed is <em>$173,000.00</em></p>\n" +
+            "<p>You earned <em>47</em> credits</p>\n";
+
+        expect(statementAsHtml(invoice, play)).toEqual(expectedStatement);
     });
 });
